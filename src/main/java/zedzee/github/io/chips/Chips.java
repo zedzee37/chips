7e1ed7a755c4f20aac76d498504921b3fa9bace7
carsonuderitz@gmail.com <carsonuderitz@gmail.com>
2025-07-28 11:19:01 -0400

    stuff

--- src/main/java/zedzee/github/io/chips/Chips.java
+++ src/main/java/zedzee/github/io/chips/Chips.java
@@ -1,10 +1,20 @@
 package zedzee.github.io.chips;
 
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.util.Identifier;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Chips implements ModInitializer {
+    public static final String MOD_ID = "chips";
+
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
     @Override
     public void onInitialize() {
+    }
+
+    public static Identifier identifier(String path) {
+        return Identifier.of(MOD_ID, path);
     }
 }
---
+++ src/main/java/zedzee/github/io/chips/mixin/BlockMixin.java
@@ -0,0 +1,21 @@
+package zedzee.github.io.chips.mixin;
+
+import net.minecraft.block.AbstractBlock;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.ShapeContext;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.shape.VoxelShape;
+import net.minecraft.util.shape.VoxelShapes;
+import net.minecraft.world.BlockView;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(AbstractBlock.class)
+public class BlockMixin {
+    @Inject(method = "getOutlineShape", at = @At("HEAD"), cancellable = true)
+    private void makeSmaller(BlockState state, BlockView world, BlockPos pos, ShapeContext context, CallbackInfoReturnable<VoxelShape> cir) {
+        cir.setReturnValue(VoxelShapes.cuboid(0.5, 0.5, 0.5, 1.0, 1.0, 1.0));
+    }
+}
