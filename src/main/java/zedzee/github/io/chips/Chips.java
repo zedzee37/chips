9b851d305b2bd00d7c594b30e9ff12c2d5f97cbc
carsonuderitz@gmail.com <carsonuderitz@gmail.com>
2025-07-28 11:04:14 -0400

    gug

---
+++ src/main/java/zedzee/github/io/chips/Chips.java
@@ -0,0 +1,10 @@
+package zedzee.github.io.chips;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Chips implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
