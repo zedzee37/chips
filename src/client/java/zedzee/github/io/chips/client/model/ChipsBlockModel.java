ee0d1b745a5fe59c75371e65f4aafb0f3d54e041
carsonuderitz@gmail.com <carsonuderitz@gmail.com>
2025-08-01 00:46:36 -0400

    gug

--- src/client/java/zedzee/github/io/chips/client/model/ChipsBlockModel.java
+++ src/client/java/zedzee/github/io/chips/client/model/ChipsBlockModel.java
@@ -1,92 +1,93 @@
 package zedzee.github.io.chips.client.model;
 
+import net.fabricmc.fabric.api.renderer.v1.mesh.MutableQuadView;
 import net.fabricmc.fabric.api.renderer.v1.mesh.QuadEmitter;
-import net.fabricmc.fabric.api.renderer.v1.model.FabricBakedModel;
-import net.fabricmc.fabric.api.renderer.v1.model.FabricBlockStateModel;
 import net.minecraft.block.BlockState;
 import net.minecraft.client.render.model.*;
-import net.minecraft.client.render.model.json.ModelTransformation;
 import net.minecraft.client.texture.Sprite;
+import net.minecraft.client.texture.SpriteAtlasTexture;
+import net.minecraft.client.util.SpriteIdentifier;
+import net.minecraft.util.Identifier;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 import net.minecraft.util.math.random.Random;
 import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.world.BlockRenderView;
 import org.jetbrains.annotations.Nullable;
-import zedzee.github.io.chips.Chips;
 import zedzee.github.io.chips.block.ChipsBlock;
 
 import java.util.List;
 import java.util.function.Predicate;
 
-public class ChipsBlockModel implements UnbakedModel, BakedModel, FabricBakedModel {
-//    @Override
-//    public void emitQuads(QuadEmitter emitter, BlockRenderView blockView, BlockPos pos, BlockState state, Random random, Predicate<@Nullable Direction> cullTest) {
-//        Chips.LOGGER.info("gug");
-//        if (!state.contains(ChipsBlock.CHIPS_PROPERTY)) {
-//            return;
-//        }
-//        Chips.LOGGER.info("bujg");
-//
-//        int shapeIdx = state.get(ChipsBlock.CHIPS_PROPERTY);
-//        VoxelShape shape = ChipsBlock.SHAPES[shapeIdx];
-//        shape.forEachBox((fromX, fromY, fromZ, toX, toY, toZ) -> {
-//            addQuads(emitter, (float) fromX, (float) fromY, (float) fromZ, (float) toX, (float) toY, (float) toZ);
-//        });
-//    }
-//
-//    private void addQuads(QuadEmitter emitter, float fromX, float fromY, float fromZ, float toX, float toY, float toZ) {
-//        emitter.square(Direction.DOWN, fromX, fromZ, toX, toZ, fromY);
-//        emitter.emit();
-//
-//        emitter.square(Direction.UP, fromX, fromZ, toX, toZ, toY);
-//        emitter.emit();
-//
-//        emitter.square(Direction.NORTH, fromX, fromY, toX, toY, fromZ);
-//        emitter.emit();
-//
-//        emitter.square(Direction.SOUTH, fromX, fromY, toX, toY, toZ);
-//        emitter.emit();
-//
-//        emitter.square(Direction.WEST, fromZ, fromY, toZ, toY, fromX);
-//        emitter.emit();
-//
-//        emitter.square(Direction.EAST, fromZ, fromY, toZ, toY, toX);
-//        emitter.emit();
-//    }
+public class ChipsBlockModel implements BlockStateModel, BlockStateModel.UnbakedGrouped {
+    private Sprite sprite;
 
     @Override
-    public List<BakedQuad> getQuads(@Nullable BlockState state, @Nullable Direction face, Random random) {
-        return List.of();
+    public void emitQuads(QuadEmitter emitter, BlockRenderView blockView, BlockPos pos, BlockState state, Random random, Predicate<@Nullable Direction> cullTest) {
+        if (!state.contains(ChipsBlock.CHIPS_PROPERTY)) {
+            return;
+        }
+
+        int shapeIdx = state.get(ChipsBlock.CHIPS_PROPERTY);
+        VoxelShape shape = ChipsBlock.SHAPES[shapeIdx];
+        shape.forEachBox((fromX, fromY, fromZ, toX, toY, toZ) -> {
+            addQuads(emitter, (float) fromX, (float) fromY, (float) fromZ, (float) toX, (float) toY, (float) toZ);
+        });
     }
 
-    @Override
-    public boolean useAmbientOcclusion() {
-        return true;
+    private void addQuads(QuadEmitter emitter, float fromX, float fromY, float fromZ, float toX, float toY, float toZ) {
+        emitter.square(Direction.DOWN, fromX, fromZ, toX, toZ, fromY);
+        emit(emitter);
+
+        emitter.square(Direction.UP, fromX, fromZ, toX, toZ, toY);
+        emit(emitter);
+
+        emitter.square(Direction.NORTH, fromX, fromY, toX, toY, fromZ);
+        emit(emitter);
+
+        emitter.square(Direction.SOUTH, fromX, fromY, toX, toY, toZ);
+        emit(emitter);
+
+        emitter.square(Direction.WEST, fromZ, fromY, toZ, toY, fromX);
+        emit(emitter);
+
+        emitter.square(Direction.EAST, fromZ, fromY, toZ, toY, toX);
+        emit(emitter);
     }
 
-    @Override
-    public boolean hasDepth() {
-        return false;
-    }
-
-    @Override
-    public boolean isSideLit() {
-        return false;
-    }
-
-    @Override
-    public Sprite getParticleSprite() {
-        return null;
-    }
-
-    @Override
-    public BakedModel bake(ModelTextures textures, Baker baker, ModelBakeSettings settings, boolean ambientOcclusion, boolean isSideLit, ModelTransformation transformation) {
-        return null;
+    private void emit(QuadEmitter emitter) {
+        emitter.spriteBake(sprite, MutableQuadView.BAKE_LOCK_UV);
+        emitter.color(-1, -1, -1, -1);
+        emitter.emit();
     }
 
     @Override
     public void resolve(Resolver resolver) {
+        resolver.markDependency(Identifier.ofVanilla("block/diamond_block"));
+    }
 
+    @Override
+    public void addParts(Random random, List<BlockModelPart> parts) {
+
+    }
+
+    @Override
+    public Sprite particleSprite() {
+        return sprite;
+    }
+
+    @Override
+    public BlockStateModel bake(BlockState state, Baker baker) {
+        ErrorCollectingSpriteGetter spriteGetter = baker.getSpriteGetter();
+
+        final SpriteIdentifier spriteIdentifier = new SpriteIdentifier(SpriteAtlasTexture.BLOCK_ATLAS_TEXTURE, Identifier.ofVanilla("block/diamond_block"));
+        this.sprite = spriteGetter.get(spriteIdentifier, () -> "");
+
+        return this;
+    }
+
+    @Override
+    public Object getEqualityGroup(BlockState state) {
+        return null;
     }
 }
--- src/client/java/zedzee/github/io/chips/client/model/ChipsModelLoadingPlugin.java
+++ src/client/java/zedzee/github/io/chips/client/model/ChipsModelLoadingPlugin.java
@@ -1,22 +1,19 @@
 package zedzee.github.io.chips.client.model;
 
 import net.fabricmc.fabric.api.client.model.loading.v1.ModelLoadingPlugin;
-import net.minecraft.client.util.ModelIdentifier;
 import net.minecraft.util.Identifier;
 import zedzee.github.io.chips.Chips;
+import zedzee.github.io.chips.block.ChipsBlocks;
 
 public class ChipsModelLoadingPlugin implements ModelLoadingPlugin {
-    public static final ModelIdentifier CHIPS_BLOCK_MODEL_ID = new ModelIdentifier(Chips.identifier("block/chips"), "");
+    public static final Identifier CHIPS_BLOCK_MODEL_ID = Chips.identifier("block/chips");
 
     @Override
     public void initialize(Context context) {
-        context.modifyModelOnLoad().register((original, ctx) -> {
-            final Identifier id = ctx.id();
-            if (id != null && id.equals(CHIPS_BLOCK_MODEL_ID)) {
-                return new ChipsBlockModel();
-            } else {
-                return original;
-            }
-        });
+        context.modifyBlockModelOnLoad().register((original, ctx) ->
+                !ctx.state().isOf(ChipsBlocks.CHIPS_BLOCK)
+                ? original
+                : new ChipsBlockModel()
+                );
     }
 }
