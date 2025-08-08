18be63b20b71fa4ad02b82829f6fe06a616c88c5
carsonuderitz@gmail.com <carsonuderitz@gmail.com>
2025-08-08 13:59:29 -0400

    add SpriteInfo for supplier and add memoize

--- src/client/java/zedzee/github/io/chips/client/model/ChipsBlockModel.java
+++ src/client/java/zedzee/github/io/chips/client/model/ChipsBlockModel.java
@@ -1,10 +1,10 @@
 package zedzee.github.io.chips.client.model;
 
+import com.google.common.base.Suppliers;
 import net.fabricmc.fabric.api.renderer.v1.mesh.MutableQuadView;
 import net.fabricmc.fabric.api.renderer.v1.mesh.QuadEmitter;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
-import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.render.model.*;
 import net.minecraft.client.texture.Sprite;
 import net.minecraft.registry.Registries;
@@ -25,16 +25,10 @@ import java.util.function.Supplier;
 
 public class ChipsBlockModel implements BlockStateModel, BlockStateModel.UnbakedGrouped {
     private static final Identifier TEMP_BLOCK_TEXTURE = Identifier.ofVanilla("block/spruce_planks");
-    private Supplier<BakedSimpleModel> modelSupplier;
-    private final Map<Direction, Sprite> spriteMap = new HashMap<>();
-    private Sprite particleSprite;
+    private Supplier<SpriteInfo> spriteSupplier;
 
     @Override
     public void emitQuads(QuadEmitter emitter, BlockRenderView blockView, BlockPos pos, BlockState state, Random random, Predicate<@Nullable Direction> cullTest) {
-        if (particleSprite == null) {
-            modelSupplier.get();
-        }
-
         if (!state.contains(ChipsBlockHelpers.CHIPS)) {
             return;
         }
@@ -108,10 +102,11 @@ public class ChipsBlockModel implements BlockStateModel, BlockStateModel.Unbaked
     }
 
     private void emit(QuadEmitter emitter, Direction direction) {
-        if (spriteMap.containsKey(direction)) {
-            emitter.spriteBake(spriteMap.get(direction), MutableQuadView.BAKE_LOCK_UV);
+        SpriteInfo info = spriteSupplier.get();
+        if (info.spriteMap().containsKey(direction)) {
+            emitter.spriteBake(info.spriteMap().get(direction), MutableQuadView.BAKE_LOCK_UV);
         } else {
-            emitter.spriteBake(particleSprite, MutableQuadView.BAKE_LOCK_UV);
+            emitter.spriteBake(info.particleSprite(), MutableQuadView.BAKE_LOCK_UV);
         }
 
         emitter.color(-1, -1, -1, -1);
@@ -127,25 +122,23 @@ public class ChipsBlockModel implements BlockStateModel, BlockStateModel.Unbaked
 
     @Override
     public Sprite particleSprite() {
-        if (particleSprite == null) {
-            modelSupplier.get();
-        }
-        return particleSprite;
+        return spriteSupplier.get().particleSprite();
     }
 
     @Override
     public BlockStateModel bake(BlockState state, Baker baker) {
 //        ErrorCollectingSpriteGetter spriteGetter = baker.getSpriteGetter();
 //
-        modelSupplier = () -> {
+        spriteSupplier = Suppliers.memoize(() -> {
             Block block = state.getBlock();
             Identifier identifier = Registries.BLOCK.getId(block);
             identifier = Identifier.of(identifier.getNamespace(), "block/" + identifier.getPath());
 
             BakedSimpleModel model = baker.getModel(identifier);
-            particleSprite = model.getParticleTexture(model.getTextures(), baker);
-            return model;
-        };
+            Sprite particleSprite = model.getParticleTexture(model.getTextures(), baker);
+            HashMap<Direction, Sprite> spriteMap = new HashMap<>();
+            return new SpriteInfo(particleSprite, spriteMap);
+        });
 
 //        final SpriteIdentifier spriteIdentifier = new SpriteIdentifier(S.BLOCK_ATLAS_TEXTURE, identifier);
 //        this.sprite = spriteGetter.get(spriteIdentifier, () -> "");
@@ -157,4 +150,6 @@ public class ChipsBlockModel implements BlockStateModel, BlockStateModel.Unbaked
     public Object getEqualityGroup(BlockState state) {
         return null;
     }
+
+    private record SpriteInfo(Sprite particleSprite, Map<Direction, Sprite> spriteMap) {}
 }
