c2cb3a2705698f96e9ff647de28500a834b2444b
carsonuderitz@gmail.com <carsonuderitz@gmail.com>
2025-08-11 17:29:24 -0400

    pick block stuff

--- src/main/java/zedzee/github/io/chips/Chips.java
+++ src/main/java/zedzee/github/io/chips/Chips.java
@@ -1,9 +1,11 @@
 package zedzee.github.io.chips;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.event.player.PlayerPickItemEvents;
 import net.minecraft.util.Identifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import zedzee.github.io.chips.block.ChipsBlockHelpers;
 import zedzee.github.io.chips.block.ChipsBlocks;
 import zedzee.github.io.chips.item.ChipsItems;
 
@@ -16,6 +18,14 @@ public class Chips implements ModInitializer {
     public void onInitialize() {
         ChipsBlocks.init();
         ChipsItems.init();
+
+        PlayerPickItemEvents.BLOCK.register((player, pos, state, requestIncludeData) ->
+            state.contains(ChipsBlockHelpers.CHIPS) ?
+                    ChipsBlockHelpers.getStackWithChips(
+                            state.getBlock().asItem().getDefaultStack(), state.get(ChipsBlockHelpers.CHIPS)
+                    )
+                    : null
+        );
     }
 
     public static Identifier identifier(String path) {
--- src/main/java/zedzee/github/io/chips/item/ChipsPattern.java
+++ src/main/java/zedzee/github/io/chips/item/ChipsPattern.java
@@ -1,15 +1,9 @@
 package zedzee.github.io.chips.item;
 
-import net.minecraft.component.ComponentMap;
 import net.minecraft.item.Item;
 
 public class ChipsPattern extends Item {
     public ChipsPattern(Settings settings) {
         super(settings);
-    }
-
-    @Override
-    public ComponentMap getComponents() {
-        return super.getComponents();
     }
 }
