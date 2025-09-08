9e84926f6804dd4667618776a55d0c3b2a812931
carsonuderitz@gmail.com <carsonuderitz@gmail.com>
2025-09-08 16:36:52 -0400

    add warning

--- src/main/java/zedzee/github/io/chips/block/CornerInfo.java
+++ src/main/java/zedzee/github/io/chips/block/CornerInfo.java
@@ -1,9 +1,17 @@
 package zedzee.github.io.chips.block;
 
+import zedzee.github.io.chips.Chips;
+
 // Standardize passing information about corners.
 public record CornerInfo(int index, int corner) {
     public static CornerInfo fromCorner(int corner) {
-        return new CornerInfo(Integer.numberOfLeadingZeros(corner) + 1, corner);
+        int index = Integer.numberOfLeadingZeros(corner);
+
+        if ((1 << index) != corner) {
+            Chips.LOGGER.warn("Non corner shape passed.");
+        }
+
+        return new CornerInfo(index, corner);
     }
 
     public static CornerInfo fromIndex(int index) {
