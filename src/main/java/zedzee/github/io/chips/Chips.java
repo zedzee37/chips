9b16e288220b2cb90c4e8919e53627d14f4925bf
carsonuderitz@gmail.com <carsonuderitz@gmail.com>
2025-09-08 15:50:23 -0400

    recipe test doesnt work make another accessor

--- src/main/java/zedzee/github/io/chips/Chips.java
+++ src/main/java/zedzee/github/io/chips/Chips.java
@@ -5,9 +5,13 @@ import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
 import net.fabricmc.fabric.api.event.player.PlayerPickItemEvents;
 import net.fabricmc.fabric.api.networking.v1.PayloadTypeRegistry;
 import net.minecraft.block.Block;
+import net.minecraft.block.Blocks;
 import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.item.Items;
 import net.minecraft.recipe.*;
 import net.minecraft.recipe.book.CraftingRecipeCategory;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.util.Identifier;
 import net.minecraft.world.World;
@@ -23,6 +27,10 @@ import zedzee.github.io.chips.item.ChipsItems;
 import zedzee.github.io.chips.mixin.PreparedRecipesAccessor;
 import zedzee.github.io.chips.networking.ChipsBlockChangePayload;
 import zedzee.github.io.chips.networking.ChiselAnimationPayload;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 public class Chips implements ModInitializer {
     public static final String MOD_ID = "chips";
@@ -40,18 +48,24 @@ public class Chips implements ModInitializer {
         PayloadTypeRegistry.playS2C().register(ChipsBlockChangePayload.ID, ChipsBlockChangePayload.CODEC);
 
         // maybe this for custom crafting?
-//        ServerTickEvents.START_SERVER_TICK.register((server) -> {
-//            ServerRecipeManager recipeManager = server.getRecipeManager();
-//            PreparedRecipesAccessor accessor = (PreparedRecipesAccessor) recipeManager;
-//
-//            PreparedRecipes recipes = accessor.getPreparedRecipes();
-//
-//            recipes.recipes().add(new RecipeEntry<CraftingRecipe>(new ShapedRecipe(
-//                    "",
-//                    CraftingRecipeCategory.MISC,
-//                    new RawShapedRecipe(3, 3)
-//            )));
-//        });
+        ServerTickEvents.START_SERVER_TICK.register((server) -> {
+            ServerRecipeManager recipeManager = server.getRecipeManager();
+            PreparedRecipesAccessor accessor = (PreparedRecipesAccessor) recipeManager;
+
+            PreparedRecipes recipes = accessor.getPreparedRecipes();
+
+            recipes.recipes().add(new RecipeEntry<CraftingRecipe>(
+                    RegistryKey.of(RegistryKeys.RECIPE, Chips.identifier("test")),
+                    new ShapedRecipe(
+                    "",
+                    CraftingRecipeCategory.MISC,
+                    new RawShapedRecipe(
+                            1, 1,
+                            List.of(Optional.of(Ingredient.ofItem(Items.DIAMOND))),
+                            Optional.empty()),
+                    ChipsBlockItem.getStack(Blocks.DIAMOND_BLOCK)
+            )));
+        });
 
         PlayerPickItemEvents.BLOCK.register(
                 (player, pos, state, requestIncludeData) -> {
