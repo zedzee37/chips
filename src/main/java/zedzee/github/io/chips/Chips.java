8001ca452315ee6ecc3dd7457c0c71720354f99a
carsonuderitz@gmail.com <carsonuderitz@gmail.com>
2025-09-08 15:19:29 -0400

    crafting experiments

--- src/main/java/zedzee/github/io/chips/Chips.java
+++ src/main/java/zedzee/github/io/chips/Chips.java
@@ -1,10 +1,14 @@
 package zedzee.github.io.chips;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
 import net.fabricmc.fabric.api.event.player.PlayerPickItemEvents;
 import net.fabricmc.fabric.api.networking.v1.PayloadTypeRegistry;
 import net.minecraft.block.Block;
 import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.recipe.*;
+import net.minecraft.recipe.book.CraftingRecipeCategory;
+import net.minecraft.server.MinecraftServer;
 import net.minecraft.util.Identifier;
 import net.minecraft.world.World;
 import org.slf4j.Logger;
@@ -16,6 +20,7 @@ import zedzee.github.io.chips.component.ChipsComponents;
 import zedzee.github.io.chips.block.ChipsBlocks;
 import zedzee.github.io.chips.item.ChipsBlockItem;
 import zedzee.github.io.chips.item.ChipsItems;
+import zedzee.github.io.chips.mixin.PreparedRecipesAccessor;
 import zedzee.github.io.chips.networking.ChipsBlockChangePayload;
 import zedzee.github.io.chips.networking.ChiselAnimationPayload;
 
@@ -33,6 +38,20 @@ public class Chips implements ModInitializer {
 
         PayloadTypeRegistry.playS2C().register(ChiselAnimationPayload.ID, ChiselAnimationPayload.CODEC);
         PayloadTypeRegistry.playS2C().register(ChipsBlockChangePayload.ID, ChipsBlockChangePayload.CODEC);
+
+        // maybe this for custom crafting?
+//        ServerTickEvents.START_SERVER_TICK.register((server) -> {
+//            ServerRecipeManager recipeManager = server.getRecipeManager();
+//            PreparedRecipesAccessor accessor = (PreparedRecipesAccessor) recipeManager;
+//
+//            PreparedRecipes recipes = accessor.getPreparedRecipes();
+//
+//            recipes.recipes().add(new RecipeEntry<CraftingRecipe>(new ShapedRecipe(
+//                    "",
+//                    CraftingRecipeCategory.MISC,
+//                    new RawShapedRecipe(3, 3)
+//            )));
+//        });
 
         PlayerPickItemEvents.BLOCK.register(
                 (player, pos, state, requestIncludeData) -> {
